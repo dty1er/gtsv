bbe22700f51480db9e16434b6e852c037eeba404
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2018-12-29 14:29:42 +0900

    refactor test as table driven test

--- gtsv_test.go
+++ gtsv_test.go
@@ -3,23 +3,55 @@ package gtsv
 import (
 	"bytes"
 	"io"
+	"reflect"
 	"testing"
 )
 
-func Test(t *testing.T) {
-	bs := bytes.NewBufferString(
-		"1\t2\t3\n" +
-			"4\t5\t6\n")
-
-	gr := New(bs)
-
-	for gr.Next() {
-		c1 := gr.Int()
-		c2 := gr.Int()
-		c3 := gr.Int()
-		t.Logf("c1=%d, c2=%d, c3=%d\n", c1, c2, c3)
+func TestInt(t *testing.T) {
+	tests := []struct {
+		name   string
+		tsv    string
+		row    int
+		col    int
+		result [][]int
+	}{
+		{
+			name: "int",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]int{[]int{1, 2, 3}, []int{4, 5, 6}},
+		},
 	}
-	if err := gr.Error(); err != nil && err != io.EOF {
-		t.Errorf("unexpected error: %s", err)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]int
+
+			for gr.Next() {
+				rowCnt++
+				var line []int
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Int())
+				}
+				ret = append(ret, line)
+			}
+
+			if err := gr.Error(); err != nil && err != io.EOF {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
 	}
+
 }
