5c0e09299d389292e631130734853141079ffe96
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2019-01-01 15:43:56 +0900

    add reading intXx/uintXx

--- gtsv.go
+++ gtsv.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"math"
 	"strconv"
 	"unsafe"
 )
@@ -76,12 +77,196 @@ func (gr *Reader) Int() int {
 		return 0
 	}
 
-	n, err := strconv.Atoi(bytesToString(b))
-	if err != nil {
-		gr.err = fmt.Errorf("cannot parse `int`: %s", err)
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil {
+		return n
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `int`: %s", s, err)
+	return 0
+}
+
+// Uint returns next uint column
+func (gr *Reader) Uint() uint {
+	if gr.err != nil {
 		return 0
 	}
-	return n
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `uint`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil && 0 <= n {
+		return uint(n)
+	}
+
+	gr.err = fmt.Errorf("cannot parse %s as `uint`: %s", s, err)
+	return 0
+}
+
+// Int8 returns next int8 column
+func (gr *Reader) Int8() int8 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `int8`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil && math.MinInt8 <= n && n <= math.MaxInt8 {
+		return int8(n)
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `int8`: %s", s, err)
+	return 0
+}
+
+// Uint8 returns next uint8 column
+func (gr *Reader) Uint8() uint8 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `uint8`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil && 0 <= n && n <= math.MaxUint8 {
+		return uint8(n)
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `uint8`: %s", s, err)
+	return 0
+}
+
+// Int16 returns next int16 column
+func (gr *Reader) Int16() int16 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `int16`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil && math.MinInt16 <= n && n <= math.MaxInt16 {
+		return int16(n)
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `int16`: %s", s, err)
+	return 0
+}
+
+// Uint16 returns next uint16 column
+func (gr *Reader) Uint16() uint16 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `uint16`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil && 0 <= n && n <= math.MaxUint16 {
+		return uint16(n)
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `uint16`: %s", s, err)
+	return 0
+}
+
+// Int32 returns next int32 column
+func (gr *Reader) Int32() int32 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `int32`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil && math.MinInt32 <= n && n <= math.MaxInt32 {
+		return int32(n)
+	}
+
+	gr.err = fmt.Errorf("cannot parse %s as `int32`: %s", s, err)
+	return 0
+}
+
+// Uint32 returns next uint32 column
+func (gr *Reader) Uint32() uint32 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `uint32`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.Atoi(s)
+	if err == nil && 0 <= n && n <= math.MaxUint32 {
+		return uint32(n)
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `uint32`: %s", s, err)
+	return 0
+}
+
+// Int64 returns next int64 column
+func (gr *Reader) Int64() int64 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `int64`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.ParseInt(s, 10, 64)
+	if err == nil && math.MinInt64 <= n && n <= math.MaxInt64 {
+		return int64(n)
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `int64`: %s", s, err)
+	return 0
+}
+
+// Uint64 returns next uint64 column
+func (gr *Reader) Uint64() uint64 {
+	if gr.err != nil {
+		return 0
+	}
+	b, err := gr.nextColumn()
+	if err != nil {
+		gr.err = fmt.Errorf("cannot read `uint64`: %s", err)
+		return 0
+	}
+
+	s := bytesToString(b)
+	n, err := strconv.ParseUint(s, 10, 64)
+
+	if err == nil && 0 <= n && n <= math.MaxUint64 {
+		return uint64(n)
+	}
+	gr.err = fmt.Errorf("cannot parse %s as `uint64`: %s", s, err)
+	return 0
 }
 
 // String returns next string column
--- gtsv_test.go
+++ gtsv_test.go
@@ -40,8 +40,648 @@ func TestInt(t *testing.T) {
 				ret = append(ret, line)
 			}
 
-			if err := gr.Error(); err != nil && err != io.EOF {
-				t.Fatalf("unexpected error: %s", err)
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestUint(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]uint
+		hasError bool
+	}{
+		{
+			name: "uint",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]uint{[]uint{1, 2, 3}, []uint{4, 5, 6}},
+		},
+		{
+			name: "contains negative number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t-1\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint{[]uint{1, 2, 3}, []uint{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint{[]uint{1, 2, 3}, []uint{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]uint
+
+			for gr.Next() {
+				rowCnt++
+				var line []uint
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Uint())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestInt8(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]int8
+		hasError bool
+	}{
+		{
+			name: "int8",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]int8{[]int8{1, 2, 3}, []int8{4, 5, 6}},
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t128\n",
+			row:      2,
+			col:      3,
+			result:   [][]int8{[]int8{1, 2, 3}, []int8{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]int8{[]int8{1, 2, 3}, []int8{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]int8
+
+			for gr.Next() {
+				rowCnt++
+				var line []int8
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Int8())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestUint8(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]uint8
+		hasError bool
+	}{
+		{
+			name: "uint8",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]uint8{[]uint8{1, 2, 3}, []uint8{4, 5, 6}},
+		},
+		{
+			name: "contains negative number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t-1\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint8{[]uint8{1, 2, 3}, []uint8{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t256\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint8{[]uint8{1, 2, 3}, []uint8{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint8{[]uint8{1, 2, 3}, []uint8{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]uint8
+
+			for gr.Next() {
+				rowCnt++
+				var line []uint8
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Uint8())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestInt16(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]int16
+		hasError bool
+	}{
+		{
+			name: "int16",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]int16{[]int16{1, 2, 3}, []int16{4, 5, 6}},
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t32768\n",
+			row:      2,
+			col:      3,
+			result:   [][]int16{[]int16{1, 2, 3}, []int16{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t65536\n",
+			row:      2,
+			col:      3,
+			result:   [][]int16{[]int16{1, 2, 3}, []int16{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]int16
+
+			for gr.Next() {
+				rowCnt++
+				var line []int16
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Int16())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestUint16(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]uint16
+		hasError bool
+	}{
+		{
+			name: "uint16",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]uint16{[]uint16{1, 2, 3}, []uint16{4, 5, 6}},
+		},
+		{
+			name: "contains negative number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t-1\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint16{[]uint16{1, 2, 3}, []uint16{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t65536\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint16{[]uint16{1, 2, 3}, []uint16{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint16{[]uint16{1, 2, 3}, []uint16{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]uint16
+
+			for gr.Next() {
+				rowCnt++
+				var line []uint16
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Uint16())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestInt32(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]int32
+		hasError bool
+	}{
+		{
+			name: "int32",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]int32{[]int32{1, 2, 3}, []int32{4, 5, 6}},
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t2147483648\n",
+			row:      2,
+			col:      3,
+			result:   [][]int32{[]int32{1, 2, 3}, []int32{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]int32{[]int32{1, 2, 3}, []int32{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]int32
+
+			for gr.Next() {
+				rowCnt++
+				var line []int32
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Int32())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestUint32(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]uint32
+		hasError bool
+	}{
+		{
+			name: "uint32",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]uint32{[]uint32{1, 2, 3}, []uint32{4, 5, 6}},
+		},
+		{
+			name: "contains negative number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t-1\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint32{[]uint32{1, 2, 3}, []uint32{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t4294967296\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint32{[]uint32{1, 2, 3}, []uint32{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint32{[]uint32{1, 2, 3}, []uint32{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]uint32
+
+			for gr.Next() {
+				rowCnt++
+				var line []uint32
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Uint32())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestInt64(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]int64
+		hasError bool
+	}{
+		{
+			name: "int64",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]int64{[]int64{1, 2, 3}, []int64{4, 5, 6}},
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t9223372036854775808\n",
+			row:      2,
+			col:      3,
+			result:   [][]int64{[]int64{1, 2, 3}, []int64{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]int64{[]int64{1, 2, 3}, []int64{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]int64
+
+			for gr.Next() {
+				rowCnt++
+				var line []int64
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Int64())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
+			}
+
+			if tt.row != rowCnt {
+				t.Fatalf("row check failed expected: %d, actual: %d", tt.row, rowCnt)
+			}
+
+			if !reflect.DeepEqual(tt.result, ret) {
+				t.Fatalf("returned value check failed expected: %v, actual: %v", tt.result, ret)
+			}
+		})
+	}
+}
+
+func TestUint64(t *testing.T) {
+	tests := []struct {
+		name     string
+		tsv      string
+		row      int
+		col      int
+		result   [][]uint64
+		hasError bool
+	}{
+		{
+			name: "uint64",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t6\n",
+			row:    2,
+			col:    3,
+			result: [][]uint64{[]uint64{1, 2, 3}, []uint64{4, 5, 6}},
+		},
+		{
+			name: "contains negative number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t-1\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint64{[]uint64{1, 2, 3}, []uint64{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains too large number",
+			tsv: "1\t2\t3\n" +
+				"4\t5\t18446744073709551616\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint64{[]uint64{1, 2, 3}, []uint64{4, 5, 0}},
+			hasError: true,
+		},
+		{
+			name: "contains string",
+			tsv: "1\t2\t3\n" +
+				"4\t5\ta\n",
+			row:      2,
+			col:      3,
+			result:   [][]uint64{[]uint64{1, 2, 3}, []uint64{4, 5, 0}},
+			hasError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gr := New(bytes.NewBufferString(tt.tsv))
+			var rowCnt int
+			var ret [][]uint64
+
+			for gr.Next() {
+				rowCnt++
+				var line []uint64
+				for i := 0; i < tt.col; i++ {
+					line = append(line, gr.Uint64())
+				}
+				ret = append(ret, line)
+			}
+			err := gr.Error()
+			if (err != nil) != tt.hasError && err != io.EOF {
+				t.Fatalf("error is not io.EOF but %s", err)
 			}
 
 			if tt.row != rowCnt {
